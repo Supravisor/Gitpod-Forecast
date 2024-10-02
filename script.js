37e85051746568da2b68df9fda183b0ef806a9aa
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-10-03 12:16:17 +1300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// date
+
+const date = new Date();
+const day = date.getDate();
+const month = date.toLocaleString("default", { month: "long" });
+
+// array for storing GitPod usage in minutes
+
+  /*  Please append to the start of the array the total minutes spent using GitPod each day.
+     On days you do not use GitPod, append 0. */
+
+let gitpodMinutes = [0, 260.1, 15.7+43.1+5.4+6.2+12.5, 29.9+12.5+9+13.2+20.5, 37.7+18.7+32, 14.9+27.9, 20.4+20.4+12.2];
